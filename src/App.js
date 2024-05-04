5d0188ac294f42369b5d0b624c63337737352d11
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2024-05-04 20:10:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("red");
+  const [text, setText] = useState("click me");
+
+  const handleClick = () => {
+    if (color === "red") {
+      setColor("blue");
+      setText("changed to blue");
+    }
+    if (color === "blue") {
+      setColor("red");
+      setText("click me");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{ backgroundColor: color }} onClick={handleClick}>
+        {text}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+const { render, screen, fireEvent } = require("@testing-library/react");
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Red-Blue Button", () => {
+  test("Button change color when clicked", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /click/i });
+    fireEvent.click(button);
+    expect(button).toHaveStyle("background-color:blue");
+  });
+
+  test("Button change text when clicked", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /click/i });
+    fireEvent.click(button);
+    expect(button).toHaveTextContent("changed to blue");
+  });
+
+  test("button has correct initial color", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /click/i });
+    expect(button).toHaveStyle("background-color:red");
+  });
+
+  test("button has initial correct text", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /click/i });
+
+    expect(button).toHaveTextContent("click me");
+  });
+});
+
+describe("Blue-Red Button", () => {
+  it("should change color when clicked", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /click/i });
+    fireEvent.click(button);
+    expect(button).toHaveStyle("background-color:blue");
+  });
+
+  it("should change text when clicked", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /click/i });
+    fireEvent.click(button);
+    expect(button).toHaveTextContent("changed to blue");
+  });
+
+  it("should has correct initial color", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /click/i });
+    expect(button).toHaveStyle("background-color:red");
+  });
+
+  it("should has initial correct text", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /click/i });
+
+    expect(button).toHaveTextContent("click me");
+  });
 });
